0cef8c227aa5183d6bae2f275d94a2c6a81442a6
Kristoffe Angelo P. Ambe <118610639+kianAmbe@users.noreply.github.com>
2023-10-23 22:55:40 +0800

    added icons

--- sadtest/index.php
+++ sadtest/index.php
@@ -3,16 +3,17 @@
 <head>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
 <div class="taskbar-container">
     <div class="taskbar">
         <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#hazard-maps">Hazard Maps</a></li>
-            <li><a href="#survival-tips">Survival Tips</a></li>
-            <li><a href="#emergency-contacts">Emergency Contacts</a></li>
+            <li><i class="fa-solid fa-house"></i><a href="#home">Home</a></li>
+            <li><i class="fa-solid fa-map"></i><a href="#hazard-maps">Hazard Maps</a></li>
+            <li><i class="fas fa-circle-info"></i><a href="#survival-tips">Survival Tips</a></li>
+            <li><i class="fas fa-address-book"></i><a href="#emergency-contacts">Emergency Contacts</a></li>
         </ul>
     </div>
 </div>
@@ -22,42 +23,21 @@
     <button class="send-report-button">Send Report</button>
 </div>
 
-
-    <div class="big-bar">
-        <!-- This is the additional bar below the header -->
-    </div>
-    <div class="content" id="hazard-maps">
-        <h2>Hazard Maps</h2>
-        <p>Explore interactive hazard maps tdfdfdfdo stay informed about potential risks in your area. Stay safe and be prepared.</p>
-    </div>
-    <div class="content" id="survival-tips">
-        <h2>Survival Tips</h2>
-        <p>Learn essential survival tips and emergency preparedness strategies. Be ready for any unexpected situationdfgd
-            dfgdfgd
-            dfgdfgddfg
-            fclosef
-            
-            fclosefg
-            datefmt_get_calendarf
-            gc_collect_cyclesdgf
-            datefmt_get_calendarffgdf
-            datefmt_get_datetypedfg
-            ddsfd
-            dfgdfgddfgdfdfdf
-            dfgdfgddfgdfdfdfdfdfd
-            dfgdfgddfgdfdfdfdfdfd
-            dfgdfgddfgdfdfdfdfdfdd
-            dfdfdfdfdfdfdklkl
-            klklkl
-            klklklklk
-            kdfdfdf
-            dfdfdfdfdfdfdklkl
-            .</p>
-    </div>
-    <div class="content" id="emergency-contacts">
-        <h2>Emergency Contacts</h2>
-        <p>Find important emergency contacts for your region, including local authorities, hospitals, and support organizations.</p>
-    </div>
+<div class="big-bar">
+    <!-- This is the additional bar below the header -->
+</div>
+<div class="content" id="hazard-maps">
+    <h2>Hazard Maps</h2>
+    <p>Explore interactive hazard maps to stay informed about potential risks in your area. Stay safe and be prepared.</p>
+</div>
+<div class="content" id="survival-tips">
+    <h2>Survival Tips</h2>
+    <p>Learn essential survival tips and emergency preparedness strategies. Be ready for any unexpected situation.</p>
+</div>
+<div class="content" id="emergency-contacts">
+    <h2>Emergency Contacts</h2>
+    <p>Find important emergency contacts for your region, including local authorities, hospitals, and support organizations.</p>
+</div>
 </body>
 </html>
 
