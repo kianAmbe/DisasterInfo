9898ee9c0eee2ed8f50cbf16537beab6399d61f5
Yorfger <2100782@lnu.edu.ph>
2023-10-24 23:09:53 +0800

    update

---
+++ sadtest/dynamic_content.php
@@ -0,0 +1,7 @@
+<?php
+
+$dynamicTitle = "Emergency Alerts and Updates";
+$dynamicMessage = "This content is generated with PHP.";
+
+
+?>
---
+++ sadtest/signup.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Placeholder Page</title>
+</head>
+<body>
+    <div style="text-align: center; margin-top: 100px;">
+        <h1>This is a placeholder</h1>
+        <p>This page is under construction.</p>
+        <button onclick="redirectToIndex()">Go back to home</button>
+    </div>
+
+    <script>
+        function redirectToIndex() {
+            window.location.href = 'index.php';
+        }
+    </script>
+</body>
+</html>
